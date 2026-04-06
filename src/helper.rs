306170a9e7dafc9b8cd5a5d15c754487a63a45ad
William Cherry <wcherry69@gmail.com>
2026-04-05 17:22:24 -0700

    Added helper functions

--- src/helper.rs
+++ src/helper.rs
@@ -1,7 +1,7 @@
 pub fn get_env_or_secret(key: &str) -> Result<String, std::env::VarError> {
-    match env::var(key) {
+    match std::env::var(key) {
         Ok(var) => Ok(var),
-        Err(_) => match env::var(format!("{}_PATH", key)) {
+        Err(_) => match std::env::var(format!("{}_PATH", key)) {
             Ok(path) =>  std::fs::read_to_string(path).map_err(|_| std::env::VarError::NotPresent),
             Err(e) => Err(e)
         },
