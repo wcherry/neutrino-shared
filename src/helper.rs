9d801b68493ed176a4cc1ae5b1fa314e62839612
William Cherry <wcherry69@gmail.com>
2026-05-15 17:20:41 -0700

    Fixing Google connection issue

--- src/helper.rs
+++ src/helper.rs
@@ -2,7 +2,7 @@ pub fn get_env_or_secret(key: &str) -> Result<String, std::env::VarError> {
     match std::env::var(key) {
         Ok(var) => Ok(var),
         Err(_) => match std::env::var(format!("{}_PATH", key)) {
-            Ok(path) =>  std::fs::read_to_string(path).map_err(|_| std::env::VarError::NotPresent),
+            Ok(path) =>  std::fs::read_to_string(path).map(|s| s.trim().to_string()).map_err(|_| std::env::VarError::NotPresent),
             Err(e) => Err(e)
         },
     }
